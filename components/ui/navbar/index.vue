603539c57a7dbb262062b36579ae9a679bf58150
hasanrana_21 <hasanrana177821@gmail.com>
2022-09-27 21:37:23 +0600

    project setup and cleanup unnecessaryfile

---
+++ components/ui/navbar/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>this is Navbar</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ui-navbar',    
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>this is main page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
